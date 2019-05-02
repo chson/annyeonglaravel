4d1736fee00a04817bf21f3d4bee0d8edbd3cf59
chson <vayu1004@gmail.com>
2019-05-02 17:19:29 +0900

    user authentication

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,30 @@
  */ 
 Route::get('/', 'WelcomeController@index');
 Route::resource('articles', 'ArticlesController');
+
+Route::get('auth/login', function() {
+	$credentials = [
+		'email'=>'john@example.com',
+		'password'=>'secret'
+	];
+
+	if(!auth()->attempt($credentials)) {
+		return '로그인 정보가 정확하지 않습니다.';
+	}
+
+	return redirect('protected');
+});
+
+Route::get('protected', ['middleware' => 'auth', function() {
+	return '어서오세요.'.auth()->user()->name;
+}]);
+
+Route::get('auth/logout', function() {
+	auth()->logout();
+
+	return 'bye';
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
